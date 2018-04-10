c7789af3f44d830972afc67886264c88742bb32a
Nazar Maksymchuk <nzrbeats@gmail.com>
2018-04-10 06:19:16 -0700

    RUns stopsd on tout

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -1,5 +1,5 @@
 <?php
-
+//@TODO: Order when order totals hit 250; free shipping
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -22,16 +22,45 @@ class ScraperController extends BaseController
     private $startingAddress = array();
     private $currentDomain = null;
     private $lastPage = 0;
+    private $excludedLinks;
+    private $products = array();
+
+
     public function run() {
         $this->client = new Client();
+        $StartTime = microtime(true);
 
         $crawler = $this->client->request('GET', 'https://www.bavauto.com/');
-        $sectionsLinks = $crawler->filter('.category-box a')->links();
-        $this->clickOnLinks($sectionsLinks);
-        echo $this->lastPage;
+        //array of objects--collection of links
+        // $sectionsLinks = $crawler->filter('.category-box a')->links();
+        $this->excludedLinks = array("/diy_videos");
+        $data = $crawler->filter('.category-box a')->each(function($node) {
+            return $this->clickOnLinks($node);
+        });
+        $data = array_filter($data, function($element) {
+            return $element != NULL;
+        });
+        // $this->clickOnLinks($sectionsLinks);
+        $EndTime = microtime(true);
+        
+        $transactionTime = ($EndTime - $StartTime);
+        echo 'time: ' .$transactionTime . 's - <br>';
+        var_dump($data);
+        // var_dump($sectionsLinks); exit;
+
+        // echo $this->lastPage;
+    }
+    private function printLog($data) {
+        foreach($data as $label => $value) {
+            if (gettype($value) != "string") {
+                continue;
+            }
+            echo "<strong>" . $label . "</strong>: " . $value . "<br>";
+        }
+        echo "<hr>";
     }
     //Does click on links
-    private function clickOnLinks($pageLinks)
+    /*private function clickOnLinks($pageLinks)
     {
         foreach ($pageLinks as $link) {
             if (!$this->isLinkDuplicate($link->getUri())) {
@@ -45,37 +74,33 @@ class ScraperController extends BaseController
                 $EndTime = microtime(true);
 
                 $transactionTime = ($EndTime - $StartTime);
-                echo 'time: ' . $transactionTime . 's - ' . 'clicked on: ' . $link->getUri() . '<br>';
-
-                $pageLinks1Deep = $crawler->filter('h2.product-name a')->links();
-                $lastPage = 0;
-                $pageNum = 0;
-                
-                $i = 0;
-                $nextUrl = $crawler->filter('h2.product-name a')->link()->getUri();
-                $lastNum = (int) explode("=", $nextUrl);
-                while ($i < $lastNum) {
-                    echo "I: ";
-                    var_dump($i);
-                    echo "</br>";
-                    echo "lastNum: ";
-                    var_dump($lastNum);
-                    echo "</br>";
-                    
-                    var_dump($i < $lastNum);
-                    echo "</br>";
-                    $crawler = $this->client->request('GET', $link->getUri());
-                    $nextUrl = $crawler->filter('h2.product-name a')->link()->getUri();
-                    $lastNum = (int) explode("=", $nextUrl);
-                    $i++;
-                    var_dump($lastNum);
-                    echo "<br>";
+                echo 'time: ' .$transactionTime . 's - ' . 'clicked on: ' . $link->getUri() . '<br>';
+                //List of Products on a specific page
+                if($crawler){
+                    $titleObj = $this->returnNode($crawler, 'div.product-name .h1');
+                    var_dump($titleObj); exit;
+                    $partNumObj = $this->returnNode($crawler, '.product-sku');
+                    if($titleObj && $partNumObj) {
+                        $product = array(
+                            "title" => $titleObj->text(),
+                            "part_num" => explode(":", $partNumObj->text())[1]
+                        );
+                        var_dump($product); exit;
+                        array_push($this->products, $product);
+                    }
                 }
-                exit;
-                $this->clickOnLinks($pageLinks1Deep);
+                $pageLinks1Deep = $crawler->filter('h2.product-name a')->links();
+                if($pageLinks1Deep) {
+                    $this->clickOnLinks($pageLinks1Deep);
+                }
+                $nextPage = $crawler->filter('a.next.i-next')->links();
+                //click on next page
+                if($nextPage) {
+                    $this->clickOnLinks($nextPage);
+                }
             }
         }
-    }
+    }*/
     private function isLinkDuplicate($url)
     {
 
@@ -109,7 +134,47 @@ class ScraperController extends BaseController
             return false;
         }
     }
-    private function clickOnProducts() {
+    private function clickOnLinks($section) {
+        $text = $section->text();
+        $link = $section->link();
+        
+        $this->printLog(array("link" => $link, "text" => $text));
+        // $this->clickOnLinks($section);
+        if(!$this->isLinkDuplicate($section->attr('href'))) {
 
+            $sectionPage = $this->client->click($link);
+            $products = $sectionPage->filter('h2.product-name a')->each(function($product, $i) {
+                $this->printLog(array("link" => $product->attr('href'), "text" => $product->text()));
+                return array("name" => $product->text(), "link" => $product->attr('href'));
+            });
+            $productsNextPage = $sectionPage->filter('a.next.i-next');
+            $nextPage = $this->clickOnLinks($productsNextPage);
+            if(!isset($nextPage)) {
+                $nextPage = array();
+            }
+            ///messy; should not always click and return the title link and products for every page 
+            return array(
+                "title" => trim($text),
+                "link" => $link,
+                "products" => array_merge($products,  $nextPage)
+            );
+        }
+    }
+    private function getProducts($linkObj) {
+        $text = $section->text();
+        $link = $section->link();
+
+        if(!$this->isLinkDuplicate($link->attr('href'))) {
+            $productsPage = $this->client->click($linkObj);
+            $products = $productsPage->filter('h2.product-name a')->each(function($product, $i) {
+                // $this->printLog(array("link" => $product->attr('href'), "text" => $product->text()));
+                return array("name" => $product->text(), "link" => $product->attr('href'));
+            });
+            $productsNextPage = $productsPage->filter('a.next.i-next');
+            $this->getProducts($productsNextPage);
+            return $products;
+        }
+        return;
+        
     }
 }
